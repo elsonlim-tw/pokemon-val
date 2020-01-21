bd55a178cceff155af85969ed5c6ea34552d726d
elsonlim-tw <elsonlim.thoughtworks.com>
2020-01-21 10:51:17 +0800

    search box

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./PokemonCard.css";
-import {PokemonType} from "./PokemonType.js";
+import { PokemonType } from "./PokemonType.js";
 import "./PokemonType.css";
 
 function PokemonCard({ pokemon }) {
@@ -8,16 +8,24 @@ function PokemonCard({ pokemon }) {
   return (
     <div className="pokemon-card">
       <div className="pokemon-card__div">
-        <img className="pokemon-card__image" src={`${process.env.PUBLIC_URL}/pokemonImage/${id}.png`} alt={name.english}></img>
+        <img
+          className="pokemon-card__image"
+          src={`${process.env.PUBLIC_URL}/pokemonImage/${id}.png`}
+          alt={name.english}
+        ></img>
       </div>
       <div className="pokemon-card__name">{name.english}</div>
       <div className="pokemon-card__type">
         {type.map(t => (
-          <PokemonType type={t}></PokemonType>
+          <PokemonType key={t} type={t}></PokemonType>
         ))}
       </div>
       {Object.entries(base).map(eachBase => {
-        return <p className="pokemon-card__base">{eachBase[0] + ": " + eachBase[1]}</p>;
+        return (
+          <p key={eachBase[0]} className="pokemon-card__base">
+            {eachBase[0] + ": " + eachBase[1]}
+          </p>
+        );
       })}
     </div>
   );
--- src/components/PokemonGallery.js
+++ src/components/PokemonGallery.js
@@ -1,23 +1,57 @@
-import React from "react"
+import React from "react";
 import pokemonData from "../pokemon/pokemon";
 import PokemonCard from "../PokemonCard";
+import SearchBox from "./SearchBox";
 import "./PokemonGallery.css";
 
-export default () => {
-    return (
-    <div>
-        <div>
-            <h1 className="pokemon_gallery__title">Pokedex</h1>
-            <br></br>
-        </div>
-        <div>
-            
-        </div>
-        <div className="pokemon-gallery">
-            {pokemonData.map(p => {
-                return <PokemonCard pokemon = {p}/>
-            })}
-        </div>
-    </div>
+class PokemonGallery extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: "",
+    };
+  }
+
+  changeSearchText = event => {
+    this.setState({
+      searchText: event.target.value,
+    });
+  };
+
+  getFilteredPokemon(pokemonData, searchText) {
+    const filteredPokemon = pokemonData.filter(aPokemon =>
+      aPokemon.name.english.toLowerCase().startsWith(searchText.toLowerCase()),
     );
+
+    return filteredPokemon;
+  }
+
+  render() {
+    const filteredPokemon = this.getFilteredPokemon(
+      pokemonData,
+      this.state.searchText,
+    );
+
+    return (
+      <div>
+        <div>
+          <h1 className="pokemon_gallery__title">Pokedex</h1>
+          <SearchBox
+            value={this.state.searchText}
+            onChange={this.changeSearchText}
+          />
+          <br></br>
+        </div>
+        <div></div>
+        <div className="pokemon-gallery">
+          {filteredPokemon.map(p => {
+            return <PokemonCard key={p.id} pokemon={p} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default PokemonGallery;
