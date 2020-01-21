5cf420e2fcb71ca76017e40b34469d52ff72fd49
elsonlim-tw <elsonlim.thoughtworks.com>
2020-01-21 10:57:44 +0800

    Updates

---
+++ precache-manifest.c41d884cdfec6bd8405f79a716e1d00e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad0a8302def0717560295941aba1a8d",
+    "url": "/pokemon-val/index.html"
+  },
+  {
+    "revision": "5b37d028557d4b1469eb",
+    "url": "/pokemon-val/static/css/main.b3d1f6bd.chunk.css"
+  },
+  {
+    "revision": "39f4b484eff3f6e89370",
+    "url": "/pokemon-val/static/js/2.4170e69c.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/pokemon-val/static/js/2.4170e69c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b37d028557d4b1469eb",
+    "url": "/pokemon-val/static/js/main.16fd8588.chunk.js"
+  },
+  {
+    "revision": "d0d027f6eb0535a24b1a",
+    "url": "/pokemon-val/static/js/runtime-main.2e18ff7e.js"
+  }
+]);
